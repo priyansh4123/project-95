cc964b90bc23ffd57750bc0a10f865b5471b8582
priyansh4123 <94119266+priyansh4123@users.noreply.github.com>
2022-03-28 19:12:40 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAuP1vNxSDp4PRtRoXbIFGdcTAu_cimNoE",
+    authDomain: "whatsapp-5a839.firebaseapp.com",
+    databaseURL: "https://whatsapp-5a839-default-rtdb.firebaseio.com",
+    projectId: "whatsapp-5a839",
+    storageBucket: "whatsapp-5a839.appspot.com",
+    messagingSenderId: "946079188991",
+    appId: "1:946079188991:web:4ed705d8433e533985c9ca"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
